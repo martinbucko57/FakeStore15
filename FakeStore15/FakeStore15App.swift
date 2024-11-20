ee880934f2fe90dd733d96adb46dbae0ae267664
Martin Bucko <martin.bucko.91@gmail.com>
2024-11-20 02:32:48 +0100

    init

---
+++ FakeStore15/FakeStore15App.swift
@@ -0,0 +1,17 @@
+//
+//  FakeStore15App.swift
+//  FakeStore15
+//
+//  Created by Martin Bucko on 20/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct FakeStore15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
